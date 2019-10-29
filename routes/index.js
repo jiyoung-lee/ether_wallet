44638d7192f679d68a3ccd6bf98cd93b4e32e5fd
jiyoung-lee <jno0717@naver.com>
2019-10-29 17:29:24 +0900

    err처리

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'INDEX' });
 });
 
+router.get('/err', function (req, res) {
+  let title = 'Error'
+  return res.render('err', { title })
+})
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt-nodejs');
 
-const db = require('../public/js/db')
+const db = require('../DB/db')
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const MySQLStore = require('express-mysql-session')(session);
 
 router.use(bodyParser.urlencoded({ extended: false }));
+
+db.connect() //mysql 접속
 
 router.use(session({
     secret: '12312dajfj23rj2po4$#%@#',
@@ -18,7 +21,7 @@ router.use(session({
         user: 'root',
         password: '111111',
         database: 'ethwallet'
-    })   
+    })
 }));
 
 router.get('/create', function (req, res) {
@@ -26,8 +29,51 @@ router.get('/create', function (req, res) {
     return res.render('create', { title });
 });
 
+/*router.post('/create_process', function (req, res) {
+    let { id, password } = req.body;
+    var sql = 'select * from wallet_info where id=?'
+    web3.eth.accounts.create();
+
+    db.query(sql, [id], function (err, results) {
+        if (err) {
+            console.log('there is no user');
+        }
+        if (!results[0]) {
+            return res.render('/error');
+        }
+
+    })
+})*/
+
 router.post('/login_process', function (req, res) {
-    var { id, password } = req.body;
+    let { id, password } = req.body;
+    var sql = 'select * from wallet_info where userid=?'
+    db.query(sql, [id], function (err, results) {
+        if (err) {
+            return res.redirect('/err')
+        }
+        if (!results.length) {
+            return res.redirect('/err')
+        }
+        else {
+            bcrypt.compare(password, results[0].password, function (err, res) {
+                if (res === true) {
+                    req.session.is_logined = true;
+                    req.session.userid = results[0].userid;
+                    req.session.password = results[0].password;
+                    req.session.public_key = results[0].public_key;
+                    req.session.private_key = results[0].private_key;
+                    req.session.save(function () {
+                        console.log('err')
+                    })
+                } else {
+                    req.session.is_logined = false;
+                    return res.redirect('/err')
+                }
+            })
+        }
+    })
+
 });
 
 
