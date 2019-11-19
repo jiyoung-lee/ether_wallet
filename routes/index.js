78045ef1830d4916b5ca2e1fb369a3580227f0b3
jiyoung-lee <jno0717@naver.com>
2019-11-19 09:45:17 +0900

    img추가

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,7 @@ router.post('/login_process', function (req, res) {
               req.session.private_key = result[0].private_key;
               req.session.public_key = result[0].public_key;
               req.session.save(function () {
+                  console.log(req.session.public_key)
                   //return res.redirect('/main')
                   return res.status(202).json({})
               });
